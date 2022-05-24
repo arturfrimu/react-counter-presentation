2f4a986675d41c328a93ff4230a19351e5edf5ed
Artur Frimu <artur.frimu2@gmail.com>
2022-05-24 19:00:38 +0300

    props

--- react-apple/src/App.js
+++ react-apple/src/App.js
@@ -5,10 +5,17 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Content from "./Content";
 
+const links = [
+    {href: '#', className:"current", label:"Home"},
+    {href: '#', className:"", label:"About"},
+    {href: '#', className:"", label:"Services"},
+    {href: '#', className:"", label:"Contact"},
+]
+
 const App = () => {
     return (
         <div>
-            <Navbar/>
+            <Navbar links={links}/>
             <Hero/>
             <Content/>
         </div>
--- react-apple/src/Navbar.jsx
+++ react-apple/src/Navbar.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 
-const links = [
-    {href: '#', className:"current", label:"Home"},
-    {href: '#', className:"", label:"About"},
-    {href: '#', className:"", label:"Services"},
-    {href: '#', className:"", label:"Contact"},
-]
-
-function Navbar() {
+function Navbar(props) {
     return <nav className="nav">
         <div className="container">
             <h1 className="logo"><a>My Website</a></h1>
             <ul>
-                {links.map(link => <li><a href={link.href} className={link.className}>{link.label}</a></li>)}
+                {props.links.map(link => <li>
+                    <a href={link.href} className={link.className}>{link.label}</a>
+                </li>)}
             </ul>
         </div>
     </nav>;
