13b531df8646f40279cc61dd30b380f73b2ea48f
Artur Frimu <artur.frimu2@gmail.com>
2022-05-24 18:58:01 +0300

    render list

--- react-apple/src/Navbar.jsx
+++ react-apple/src/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
+const links = [
+    {href: '#', className:"current", label:"Home"},
+    {href: '#', className:"", label:"About"},
+    {href: '#', className:"", label:"Services"},
+    {href: '#', className:"", label:"Contact"},
+]
+
 function Navbar() {
     return <nav className="nav">
         <div className="container">
             <h1 className="logo"><a>My Website</a></h1>
             <ul>
-                <li><a href="#" className="current">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
+                {links.map(link => <li><a href={link.href} className={link.className}>{link.label}</a></li>)}
             </ul>
         </div>
     </nav>;
